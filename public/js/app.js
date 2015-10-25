8d19e1a7bf3c9aa349931bb68ee67617a2ff794f
Chris <chris@boddy.im>
2015-10-25 21:44:21 +0000

    added day-of-week button-group.

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,30 @@
+
+var AlarmForm = React.createClass({
+        toLabel: function(day) {
+                return (<label className="btn btn-default">
+                                <input type="checkbox" value={day}>
+                                {day}
+                                </input>
+                                </label>);
+        },
+        render : function() {
+                return (<div>
+                                <div className="btn-group" data-toggle="buttons">
+                                {
+                                        ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map(this.toLabel)
+                                }
+                                </div>
+                                </div>);
+        }
+});
+
+
+
 var App  = React.createClass({
 
         render: function() {
                 return (<div>
-                                <p>Hello World</p>
+                                <AlarmForm/>
                                 </div>)
         }
 });
