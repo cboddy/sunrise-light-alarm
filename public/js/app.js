f0431ae55a622575727877aa1310dbc8c41c0769
Chris <chris@boddy.im>
2015-10-25 22:57:31 +0000

    added form for setting alarm time  and  days-of-week.

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,40 @@
 
 var AlarmForm = React.createClass({
-        toLabel: function(day) {
+        toDayLabel: function(day) {
                 return (<label className="btn btn-default">
                                 <input type="checkbox" value={day}>
                                 {day}
                                 </input>
                                 </label>);
         },
+        componentDidMount: function() {
+                $("#timepicker").timepicker();
+        },
         render : function() {
-                return (<div>
-                                <div className="btn-group" data-toggle="buttons">
+                return (<div className="col-sm-6">
+                                <center>
+                                <form >
+                                <div className="control-group">
+                                <label htmlFor="timepicker-wrapper" className="col-sm-3 control-label">Alarm time</label>
+                                <div id="timepicker-wrapper" className="bootstrap-timepicker timepicker col-sm-9">
+                                <input id="timepicker" type="text" className="form-control text-center input-lg">
+                                </input>
+                                </div>
+                                </div>
+
+                                <div className="control-group">
+                                <label htmlFor="daysOfWeek" className="col-sm-3 control-label">Days of week</label>
+                                <div id="daysOfWeek" className="col-sm-9 btn-group" data-toggle="buttons" id="dayOfWeek">
                                 {
-                                        ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map(this.toLabel)
+                                        ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"].map(this.toDayLabel)
                                 }
                                 </div>
+                                </div>
+                                <div className="control-group">
+                                <button  type="submit" className="btn btn-default">Set alarm</button>
+                                </div>
+                                </form>
+                                </center>
                                 </div>);
         }
 });
