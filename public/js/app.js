0a7a3b4c0242df9f87b10f29be09bd61309922da
Chris <chris@boddy.im>
2015-10-31 13:34:15 +0000

    added other controllers.

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 
 var AlarmForm = React.createClass({
         toDayLabel: function(day) {
-                return (<label className="btn btn-default">
+                return (<label key={day} className="btn btn-default">
                                 <input type="checkbox" value={day}>
                                 {day}
                                 </input>
@@ -11,30 +11,25 @@ var AlarmForm = React.createClass({
                 $("#timepicker").timepicker();
         },
         render : function() {
-                return (<div className="col-sm-6">
-                                <center>
-                                <form >
-                                <div className="control-group">
-                                <label htmlFor="timepicker-wrapper" className="col-sm-3 control-label">Alarm time</label>
-                                <div id="timepicker-wrapper" className="bootstrap-timepicker timepicker col-sm-9">
-                                <input id="timepicker" type="text" className="form-control text-center input-lg">
-                                </input>
+                return (<div>
+                                <form role="form-inline"> 
+                                <div className="form-group">
+                                <div className="input-group bootstrap-timepicker timepicker">
+                                <input id="timepicker" type="text" className="text-center input-lg"></input>
                                 </div>
                                 </div>
 
-                                <div className="control-group">
-                                <label htmlFor="daysOfWeek" className="col-sm-3 control-label">Days of week</label>
-                                <div id="daysOfWeek" className="col-sm-9 btn-group" data-toggle="buttons" id="dayOfWeek">
+                                <div className="form-group">
+                                <div id="daysOfWeek" className="btn-group" data-toggle="buttons" id="dayOfWeek">
                                 {
-                                        ["M", "T", "W", "T", "F", "Sa", "Su"].map(this.toDayLabel)
+                                        ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"].map(this.toDayLabel)
                                 }
                                 </div>
                                 </div>
-                                <div className="control-group">
+
                                 <button  type="submit" className="btn btn-default">Set alarm</button>
-                                </div>
                                 </form>
-                                </center>
+
                                 </div>);
         }
 });
@@ -45,9 +40,24 @@ var App  = React.createClass({
 
         render: function() {
                 return (<div>
+                                <center>
+                                <h2>Rise and Shine</h2>
                                 <AlarmForm/>
+                                <div className="btn-group-vertical">
+                                <button ref="unset" className="btn btn-default">Un-set alarm</button>
+                                <button ref="test" className="btn btn-default">Test-lights</button>
+                                <button ref="addMusic" className="btn btn-default">Add music</button>
+                                </div>
+                                </center>
+
                                 </div>)
+        },
+
+        componentDidMount: function() {
+                React.findDOMNode(this.refs.test);
         }
+
+        
 });
 
 React.render(
