d4a2daa1a6c56a0aa0928747e06797a68c35c13a
Chris <chris@boddy.im>
2015-10-31 16:06:30 +0000

    more alarm setting.

--- alarm.py
+++ alarm.py
@@ -1,8 +1,24 @@
 import threading
 import datetime
 import time
+
+seconds_per_day  = 60*60*24
+
+def getLight(deltaSeconds):
+    deltaMinutes = deltaSeconds / 60
+    red  =  1.0 - min(0, (deltaMinutes. - 50)/10)
+    green = min(deltaMinutes - 10, 0)  
+"""
+    delta: number of seconds before alarm time
+    generate r,g,b values based on alarm
+"""
+    pass
+
+
+
+
 class Alarm(threading.Thread):
-    def __init(self, timeOfDay, daysOfWeek, delay=10):
+    def __init(self, timeOfDay, daysOfWeek, delay=10, getLight):
         self.timeOfDay = timeOfDay
         self.daysOfWeek = daysOfWeek
         self.delay = delay
@@ -12,13 +28,15 @@ class Alarm(threading.Thread):
         while not isFinished:
             time.sleep(delay)
             dt = datetime.datetime.now()
-            light = getLight(dt) 
-            setLight(light)
+            if not dt.weekday() in self.daysOfWeek:
+                continue
+
+            delta =  self.timeOfDay - dt
+            delta = delta.seconds  % seconds_per_day
+            light = getLight(delta) 
+            self.setLight(light)
 
     def setLight(self, light):
-        pass
-
-    def getLight(self, dateTime):
         pass
 
     def close(self):
