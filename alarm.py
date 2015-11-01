2b900d60f0e12d707c67a264f65c0dd77c1217f8
Chris <chris@boddy.im>
2015-11-01 09:01:54 +0000

    implementedalarm light level selection based on date-time.

--- alarm.py
+++ alarm.py
@@ -2,39 +2,47 @@ import threading
 import datetime
 import time
 
-seconds_per_day  = 60*60*24
-
-def getLight(deltaSeconds):
-    deltaMinutes = deltaSeconds / 60
-    red  =  1.0 - min(0, (deltaMinutes. - 50)/10)
-    green = min(deltaMinutes - 10, 0)  
-"""
-    delta: number of seconds before alarm time
-    generate r,g,b values based on alarm
-"""
-    pass
-
-
+seconds_per_minute  = 60
+seconds_per_day  = seconds_per_minute*60*24
+minutes_per_day  = seconds_per_day / 60 
 
 
 class Alarm(threading.Thread):
-    def __init(self, timeOfDay, daysOfWeek, delay=10, getLight):
+    def __init(self, timeOfDay, daysOfWeek, wakeUpMinutes=30, graceMinutes=10,delay=10):
         self.timeOfDay = timeOfDay
         self.daysOfWeek = daysOfWeek
         self.delay = delay
+        self.wakeUpMinutes = wakeUpMinutes
+        self.graceMinutes = graceMinutes
         self.__isFinished = False
+
+    """
+        deltaMinutes: number of minutes before alarm time
+        generate r,g,b values 
+    """
+    def  getLight(self, deltaMinutes):
+        if minutes_per_day - deltaMinutes < self.graceMinutes:
+            return 1.0,1.0,1.0
+        if deltaMinutes > self.wakeUpMinutes:
+            return None 
+        ramp, full = self.wakeUpMinutes/3, self.wakeUpMinutes*4/5
+        inverse_redness = max(0, deltaMinutes - ramp)
+        red  =  1.0 - inverse_redness
+        inverse_other = max(0, deltaMinutes -full)
+        green =  blue =  1.0 - inverse_other
+        return  red,green,blue
         
     def run(self):
         while not isFinished:
             time.sleep(delay)
             dt = datetime.datetime.now()
-            if not dt.weekday() in self.daysOfWeek:
-                continue
+            if not dt.weekday() in self.daysOfWeek: continue
 
             delta =  self.timeOfDay - dt
-            delta = delta.seconds  % seconds_per_day
+            deltaMinutes = (delta.seconds  % seconds_per_day) / seconds_per_minute
             light = getLight(delta) 
-            self.setLight(light)
+            print("light", light)
+            if light is not None: self.setLight(light)
 
     def setLight(self, light):
         pass
